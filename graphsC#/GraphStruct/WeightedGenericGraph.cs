c16f00b5f470851df02dc522572d683987b44844
Derek Deming <derekdeming17@gmail.com>
2024-12-27 11:48:41 -0500

    weighted generic graph

---
+++ graphsC#/GraphStruct/WeightedGenericGraph.cs
@@ -0,0 +1,42 @@
+﻿using Priority_Queue;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphStruct
+{
+    #region WeightedGraphNode: Create node structure
+    public class WeightedGenericGraph<T>
+    {
+        #region Properties - Index, Value, Neighbors, Weights
+        public int Index { get; set; }
+        public T Value { get; set; }
+        public List<WeightedGraphNode<T>> Neighbors { get; set; } = new List<WeightedGraphNode<T>>();
+        public List<int> Weights { get; set; } = new List<int>();
+        #endregion
+        #region Basic Operations - AddNeighbors, RemoveNeighbors, ToString
+        public bool AddNeighbors(WeightedGenericGraph<T> neighbor)
+        {
+            if (Neighbors.Contains(neighbor))
+                return false;
+            else
+            {
+                Neighbors.Add(neighbor);
+                return true;
+            }
+        }
+        //public bool RemoveNeighbors(WeightedGraphNode<T> neighbor)
+        //{
+        //    return Neighbors.Remove(neighbor);
+        //}
+
+        //public bool RemoveAllNeighbors()
+        //{
+        //    for (int i = Neighbors.Count; i >= 0; i--)
+        //    {
+        //        Neighbors.RemoveAt(i);
+        //    }
+        //    return true;
+        //}
+    }
+}
